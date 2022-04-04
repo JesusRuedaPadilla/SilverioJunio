31b2b51afc503701bd43d240eb2fa660c9cc7019
Jesus Rueda Padilla <jruepad413@g.educaand.es>
2022-04-04 13:00:16 +0200

    Ejemplo devolviendo hora con php

--- AJAX/js/ejemplo1AJAX.js
+++ AJAX/js/ejemplo1AJAX.js
@@ -14,3 +14,22 @@ window.addEventListener("load",function(){
     document.getElementsByTagName("button")[0].onclick=cargarMensajes;
     window.setInterval(cargarMensajes,4000);
 });
+
+function cargarHora(){
+    var ajax=new XMLHttpRequest();
+    ajax.onreadystatechange=function(){
+        if(ajax.readyState==4 && ajax.status==200){
+            document.getElementById("hora").innerHTML=ajax.responseText;
+        }
+    }
+    ajax.open("GET","hora.php");
+    ajax.send();
+}
+
+window.addEventListener("load",function(){
+
+    // document.getElementsByTagName("button")[0].onclick=cargarMensajes;
+    window.setInterval(cargarHora,1000);
+});
+
+
