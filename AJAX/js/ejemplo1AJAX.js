c9bcaabb1f28f08c0809dac276ba6537b88e83f6
Jesus Rueda Padilla <jruepad413@g.educaand.es>
2022-04-04 13:29:26 +0200

    AJAX con array en listadoAlumnos

--- AJAX/js/ejemplo1AJAX.js
+++ AJAX/js/ejemplo1AJAX.js
@@ -9,11 +9,6 @@ function cargarMensajes(){
     ajax.send();
 }
 
-window.addEventListener("load",function(){
-
-    document.getElementsByTagName("button")[0].onclick=cargarMensajes;
-    window.setInterval(cargarMensajes,4000);
-});
 
 function cargarHora(){
     var ajax=new XMLHttpRequest();
@@ -26,10 +21,32 @@ function cargarHora(){
     ajax.send();
 }
 
+
+
+function cargarAlumnos(){
+    var ajax=new XMLHttpRequest();
+    ajax.onreadystatechange=function(){
+        if(ajax.readyState==4 && ajax.status==200){
+            var vector=JSON.parse(ajax.responseText);
+            var listado= document.getElementById("listado");
+            listado.innerHTML="";
+            for(let i=0;i<vector.length;i++){
+                var li=document.createElement("li");
+                li.innerHTML=vector[i];
+                listado.appendChild(li);
+            }
+        }
+    }
+    ajax.open("GET","listadoAlumnos.txt");
+    ajax.send();
+}
+
 window.addEventListener("load",function(){
 
-    // document.getElementsByTagName("button")[0].onclick=cargarMensajes;
+    document.getElementsByTagName("button")[0].onclick=cargarMensajes;
+    window.setInterval(cargarMensajes,4000);
     window.setInterval(cargarHora,1000);
+    window.setInterval(cargarAlumnos,5000);
 });
 
 
